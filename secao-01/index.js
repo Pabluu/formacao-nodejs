51e22e948c51fcdbd2a6a09e3b05175fb28ab869
Pablo Henrique <pabloph0077@gmail.com>
2022-10-09 12:39:28 -0300

    pasteando arquivos

---
+++ secao-01/index.js
@@ -0,0 +1,14 @@
+let calculadora = require('./calculadora')
+
+console.log(calculadora.soma(123,20))
+console.log(calculadora.sub(123,20))
+
+
+/*let mostrarSite = true;
+const site = 'www.guiadoprogramador.com'
+
+console.log('Hello World');
+
+if(mostrarSite){
+    console.log(site)
+}*/
