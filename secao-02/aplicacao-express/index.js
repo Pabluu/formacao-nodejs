b2fb62a38a34919b73b9fa77a2e4fc5ffaeecd69
Pablo Henrique <pabloph0077@gmail.com>
2022-10-09 16:46:00 -0300

    parametros opcionais e obrigatorios

--- secao-02/aplicacao-express/index.js
+++ secao-02/aplicacao-express/index.js
@@ -3,19 +3,31 @@ const express = require('express'); //importando o express
 const app = express(); // startando o express
 
 
-// criado uma rota
+// rota 'home'
 app.get('/', function(req, res){
     res.send('Bem-vindo ao guia do programador') 
     // irá dar um erro pois só pode enviar uma resposta por requisição
     // res.send('Bem-vindo ao guia do programador') // ERR_HTTP_HEADERS_SENT
 });
 
+// rota 'blog'
 app.get('/blog', function(req, res){
     res.send('Bem-vindo ao meu blog');
 })
 
+// rota 'canal/youtube'
 app.get('/canal/youtube', function(req, res){
     res.send('<h1>Bem-vindo ao meu canal</h1>');
+})
+
+// parametro OPCIONAL
+app.get('/blog/:artigo?', function(req, res){
+    res.send(`Bem-vindo ao artigo ${req.params['artigo']}`)
+})
+
+// parametro OBRIGATORIO
+app.get('/ola/:nome', function(req, res){
+    res.send(`Oiii, ${req.params.nome}`)
 })
 
 // servidor ouvindo na porta 4000
