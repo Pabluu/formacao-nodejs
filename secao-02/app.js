9c377b5f29022acb182bdf638adfe1888b98146a
Pablo Henrique <pabloph0077@gmail.com>
2022-10-09 15:23:29 -0300

    criando projeto com express

--- secao-02/app.js
+++
@@ -1,6 +0,0 @@
-let http = require('http'); //modulo http já vem por padrão no node
-
-http.createServer(function(requisicao, resposta){
-    resposta.end("BEM VINDO AO SITE!!!")
-}).listen(8181);
-console.log("Meu servidor está rodando...")
