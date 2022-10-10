fd0e7ef141f3578deca0a638948232ab194b67cf
Pablo Henrique <pabloph0077@gmail.com>
2022-10-10 11:19:07 -0300

    trabalhando com condicionais e loops

--- secao-05-proj-01/guia-perguntas/index.js
+++ secao-05-proj-01/guia-perguntas/index.js
@@ -4,16 +4,26 @@ const app = express();
 // setando o motor de html é o EJS
 app.set('view engine', 'ejs');
 
-app.get('/:nome?', (req, res) =>{
+app.get('/:nome?', (req, res) => {
     // res.send('Besm-vinda(o) ao meu site')
     let nome = req.params['nome'] || 'Pablo Henrique';
-    res.render('index', {nome});
+    let exibirMsg = true;
+
+
+    let produtos = [
+        { nome: 'Doritos', preco: 3.14 },
+        { nome: 'Leite', preco: 5.90 },
+        { nome: 'Pão', preco: 0.5 },
+    ]
+
+    res.render('index',
+        { nome, msg: exibirMsg, produtos: produtos });
 })
 
 app.listen(8080, (error) => {
-    if(error){
+    if (error) {
         console.log(`Erro ao levantar o servidor: ${error}`)
-    } else{
+    } else {
         console.log("Servidor no ar!!!");
     }
 });
