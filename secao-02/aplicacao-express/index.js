e97dfc431b20ea32e138bd3db0d68915f894dc57
Pablo Henrique <pabloph0077@gmail.com>
2022-10-09 22:51:00 -0300

    query params

--- secao-02/aplicacao-express/index.js
+++ secao-02/aplicacao-express/index.js
@@ -4,37 +4,43 @@ const app = express(); // startando o express
 
 
 // rota 'home'
-app.get('/', function(req, res){
-    res.send('Bem-vindo ao guia do programador') 
+app.get('/', function (req, res) {
+    res.send('Bem-vindo ao guia do programador')
     // irá dar um erro pois só pode enviar uma resposta por requisição
     // res.send('Bem-vindo ao guia do programador') // ERR_HTTP_HEADERS_SENT
 });
 
 // rota 'blog'
-app.get('/blog', function(req, res){
+app.get('/blog', function (req, res) {
     res.send('Bem-vindo ao meu blog');
 })
 
 // rota 'canal/youtube'
-app.get('/canal/youtube', function(req, res){
-    res.send('<h1>Bem-vindo ao meu canal</h1>');
+app.get('/canal/youtube', function (req, res) {
+    let canal = req.query['canal'];
+
+    if (canal) {
+        res.send(`<h1>Bem-vindo ao canal ${canal}</h1>`);
+    } else{
+        res.send('Nenhum canal fornecido');
+    }
 })
 
 // parametro OPCIONAL
-app.get('/blog/:artigo?', function(req, res){
+app.get('/blog/:artigo?', function (req, res) {
     res.send(`Bem-vindo ao artigo ${req.params['artigo']}`)
 })
 
 // parametro OBRIGATORIO
-app.get('/ola/:nome', function(req, res){
+app.get('/ola/:nome', function (req, res) {
     res.send(`Oiii, ${req.params.nome}`)
 })
 
 // servidor ouvindo na porta 4000
-app.listen(4000, function(error){
-    if(error){
+app.listen(4000, function (error) {
+    if (error) {
         console.log('Ocorreu um erro');
-    } else{
+    } else {
         console.log('Servidor rodando...')
     }
 })
