dee81a278b5cfd333c9ba3db926ac9524a82a841
Pablo Henrique <pabloph0077@gmail.com>
2022-10-11 12:23:27 -0300

    paginas estáticas

--- secao-05-proj-01/guia-perguntas/index.js
+++ secao-05-proj-01/guia-perguntas/index.js
@@ -3,21 +3,15 @@ const app = express();
 
 // setando o motor de html é o EJS
 app.set('view engine', 'ejs');
+// arquivos estaticos
+app.use(express.static('public'));
 
-app.get('/:nome?', (req, res) => {
-    // res.send('Besm-vinda(o) ao meu site')
-    let nome = req.params['nome'] || 'Pablo Henrique';
-    let exibirMsg = true;
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
-
-    let produtos = [
-        { nome: 'Doritos', preco: 3.14 },
-        { nome: 'Leite', preco: 5.90 },
-        { nome: 'Pão', preco: 0.5 },
-    ]
-
-    res.render('index',
-        { nome, msg: exibirMsg, produtos: produtos });
+app.get('/perguntar', (req, res) => {
+    res.render('perguntar');
 })
 
 app.listen(8080, (error) => {
