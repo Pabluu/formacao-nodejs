a043601d676ca1609c46a22bdf7a79bd786b5c25
Pablo Henrique <pabloph0077@gmail.com>
2022-10-12 15:12:04 -0300

    capturando dados do formulário

--- secao-05-proj-01/guia-perguntas/index.js
+++ secao-05-proj-01/guia-perguntas/index.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 // setando o motor de html é o EJS
 app.set('view engine', 'ejs');
 // arquivos estaticos
 app.use(express.static('public'));
 
+// traduzir o conteudo para que seja manipulável.
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+// ROTAS
 app.get('/', (req, res) => {
     res.render('index');
 });
 
 app.get('/perguntar', (req, res) => {
     res.render('perguntar');
+})
+
+
+app.post('/salvarpergunta', (req, res) => {
+    let titulo = req.body.titulo || '*SEM TÍTULO*';
+    let descricao = req.body.descricao || '*SEM DESCRIÇÃO*';
+
+    res.send(`Formulário recebido!!! Título ${titulo} Descrição: ${descricao}`);
 })
 
 app.listen(8080, (error) => {
