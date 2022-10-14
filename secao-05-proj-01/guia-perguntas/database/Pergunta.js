bfba069eb52be2395fa57195bb92de8245b529ef
Pablo Henrique <pabloph0077@gmail.com>
2022-10-13 22:02:16 -0300

    criando um model

---
+++ secao-05-proj-01/guia-perguntas/database/Pergunta.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize');
+const connection = require('./database');
+
+const Pergunta = connection.define('pergunta', {
+    titulo: {type: Sequelize.STRING, allowNull: false},
+    descricao: {type:Sequelize.TEXT, allowNull: false}
+});
+
+Pergunta.sync({force: false})
+.then(() => {
+    console.log('Tabela "Pergunta" criada');
+})
+
+
+
+module.exports = Pergunta;
