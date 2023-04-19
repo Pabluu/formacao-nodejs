ed93a53ed63657fd7fe9db899af55bc7cb1c2137
Pablo Henrique SC <pabloph0077@gmail.com>
2023-04-18 22:04:33 -0300

    insert em bd

--- secao-19-knex.js-sql-avancado/index.js
+++ secao-19-knex.js-sql-avancado/index.js
@@ -1,1 +1,29 @@
 const database = require("./database");
+
+// INSERT
+/* let dados = [
+  {
+    nome: "Call of Duty 2",
+    preco: 86.8,
+  },
+  {
+    nome: "GTA V",
+    preco: 105.99,
+  },
+  {
+    nome: "The Witcher",
+    preco: 16.9,
+  },
+];
+
+database
+  .insert(dados)
+  .into("games")
+
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+*/
