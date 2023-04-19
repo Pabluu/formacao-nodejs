92709a7b5d148ee7eaab40723092e7b6a58e8692
Pablo Henrique SC <pabloph0077@gmail.com>
2023-04-18 22:04:19 -0300

    alterando nome das variaveis de ambiente

--- secao-19-knex.js-sql-avancado/database.js
+++ secao-19-knex.js-sql-avancado/database.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
-const { PORT, USER, PASSWORD, DATABASE } = process.env;
+
+const { PORTDB, USERDB, PASSDB, DATABASE } = process.env;
 
 const knex = require("knex")({
   client: "mysql2",
   connection: {
     host: "127.0.0.1",
-    port: PORT,
-    user: USER,
-    password: PASSWORD,
+    port: PORTDB,
+    user: USERDB,
+    password: PASSDB,
     database: DATABASE,
   },
 });
