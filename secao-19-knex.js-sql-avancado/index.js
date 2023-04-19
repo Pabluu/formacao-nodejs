ccdef8ff68b5cf78bafa6d8a3745f6f5067d32b7
Pablo Henrique SC <pabloph0077@gmail.com>
2023-04-18 22:16:51 -0300

    Trabalhando com Select e Nested Queries

--- secao-19-knex.js-sql-avancado/index.js
+++ secao-19-knex.js-sql-avancado/index.js
@@ -1,19 +1,15 @@
 const database = require("./database");
 
 // INSERT
-/* let dados = [
-  {
-    nome: "Call of Duty 2",
-    preco: 86.8,
-  },
-  {
-    nome: "GTA V",
-    preco: 105.99,
-  },
-  {
-    nome: "The Witcher",
-    preco: 16.9,
-  },
+
+/*
+let dados = [
+  { nome: "Sea of Thieves", preco: 50.97 },
+  { nome: "Call Of Duty", preco: 50 },
+  { nome: "Rocket League", preco: 0 },
+  { nome: "Call of Duty 2", preco: 86.8 },
+  { nome: "GTA V", preco: 105.99 },
+  { nome: "The Witcher", preco: 16.9 },
 ];
 
 database
@@ -22,6 +18,39 @@ database
 
   .then((data) => {
     console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+  */
+
+// SELECT
+/* 
+  database.select(["nome", "preco"])
+  .table("games")
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+*/
+
+//NESTED QUERIES
+
+/* database.insert({ nome: "Mists of noyah", preco: 25 })
+  .into("games")
+  .then((data) => {
+    database
+      .select(["nome", "preco"])
+      .table("games")
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   })
   .catch((error) => {
     console.log(error);
